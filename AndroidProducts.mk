b3aef00b24ee9fc1d18f8d4b6e855002c5d0ef5e
VitorSSSouza <vitorsantosenfo@icloud.com>
2021-07-05 21:19:43 -0300

    lake: legion soon

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lake.mk
+    $(LOCAL_DIR)/legion_lake.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lake-eng \
-    aosp_lake-user \
-    aosp_lake-userdebug
+    legion_lake-eng \
+    legion_lake-user \
+    legion_lake-userdebug
